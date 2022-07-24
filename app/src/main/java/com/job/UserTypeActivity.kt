52143f31253b19769db2b908dd9fe83b8526bbdf
martinsfelipemartins <martins.felipe1@hotmail.com>
2022-07-24 02:46:19 -0300

    cria projeto app search job

---
+++ app/src/main/java/com/job/UserTypeActivity.kt
@@ -0,0 +1,11 @@
+package com.job
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserTypeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
