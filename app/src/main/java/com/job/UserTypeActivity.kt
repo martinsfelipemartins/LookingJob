f3357b45b6731b7e3d7e8f1b0eb76b9df6820615
martinsfelipemartins <martins.felipe1@hotmail.com>
2022-08-04 23:53:27 -0300

    criação layout

--- app/src/main/java/com/job/UserTypeActivity.kt
+++ app/src/main/java/com/job/UserTypeActivity.kt
@@ -1,11 +1,17 @@
 package com.job
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+ import android.os.Bundle
+ import androidx.appcompat.app.AppCompatActivity
+ import com.job.databinding.UserTypeActivityBinding
 
 class UserTypeActivity : AppCompatActivity() {
+
+    private lateinit var binding: UserTypeActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding =  UserTypeActivityBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
